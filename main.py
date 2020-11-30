5c3c42f63e503d1e3cb1da4f54bcb4c4152a8bdb
lidyapradina <67100523+lidyapradina@users.noreply.github.com>
2020-11-30 08:30:09 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name": "My Test",
+    "blender": (2, 79, 0),
+    "category": "Object",
+}
+def register():
+    print("Hello World")
+def unregister():
+    print("Goodbye World")
